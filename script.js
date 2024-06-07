4301cbaa1f79343e20e6a43c876a7538f98fda04
fairleyv <fairleyvincent@gmail.com>
2024-06-07 00:30:44 -0700

    updated the code for apply classes to include adding present and future classes

--- script.js
+++ script.js
@@ -30,10 +30,14 @@ saveBtns.on("click", function() {
   $(".time-block").each(function() {
     var hourBlock = parseInt($(this).attr("id").split("-")[1]);
 
-    $(this).removeClass("past prestent future");
+    $(this).removeClass("past present future");
 
     if (hourBlock < currentHour) {
       $(this).addClass("past");
+    } else if (hourBlock === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
     }
   });
   //
